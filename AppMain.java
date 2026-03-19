0a60983b2645291fffcd88fd572691a02e652b06
VicenEducaM <vcm803@educa.madrid.org>
2026-03-19 17:48:44 +0100

    Update AppMain.java
    Incorporada la prueba de Calculadora

--- AppMain.java
+++ AppMain.java
@@ -2,5 +2,9 @@ public class AppMain {
     public static void main(String[] args) {
         Saludador saludador = new Saludador();
         saludador.saludos();
+ 
+        Calculadora calc = new Calculadora();
+        int resultado = calc.multiplicar(3, 4);
+        System.out.println("Resultado: " + resultado);       
 }
 }
